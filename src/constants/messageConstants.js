a06e501f4711ac21779b3b86984eb795ef946df8
vyaspriyank <priyankvyas05@gmail.com>
2023-03-27 22:41:42 +0530

    initial commit

---
+++ src/constants/messageConstants.js
@@ -0,0 +1,7 @@
+export const messageConstant = {
+    insertMessage : "New User Created Successfully",
+    editMessage : "User edited Successfully",
+    deleteMessage : "User Deleted Successfully",
+    confirmDelete: `Are you sure you want to delete?`,
+    deleteErrorMsg: `There was an error deleting the user`
+}
---
+++ src/constants/urlConstant.js
@@ -0,0 +1,6 @@
+export const urlConstant = {
+    createAPI : "https://reqres.in/api/users",
+    editAPI : "https://reqres.in/api/users/",
+    deleteAPI : "https://reqres.in/api/users/",
+    getAllAPI : "https://reqres.in/api/users?page=1",      
+}
